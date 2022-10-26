0665d711c9e709e10f98ef2f81dcc4925a357175
Tarikulk <ullahtarik300@gmail.com>
2022-10-26 07:34:43 +0600

    header nav added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -8,7 +8,16 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-           
+            <Navbar bg="dark" variant="dark">
+        <Container className='nav-container'>
+          <Link to="/">Courses-Hub</Link>
+          <Nav className="me-auto navbar">
+             <Link to='/'>Home</Link>
+             <Link to='/faq'>FAQ</Link>
+             <Link to='/blog'>Blog</Link>
+          </Nav>
+        </Container>
+      </Navbar> 
         </div>
     );
 };
