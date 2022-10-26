0a05d518ca2b69c5145e5711a3dd9dc8a38afac3
Tarikulk <ullahtarik300@gmail.com>
2022-10-26 09:34:52 +0600

    register authentication set up added

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { createContext } from 'react';
-import {getAuth} from "firebase/auth";
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, updateProfile} from "firebase/auth";
+import app from '../firebase/firebase.config';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const AuthContext = createContext();
-const auth = getAuth()
+const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
-    const user = {name: "Obayedullah Khan"}
-    const authInfo = {user}
+    const [user, setUser] = useState();
+
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (profile) =>{
+        return updateProfile(auth.currentUser, profile)
+    }
+
+    useEffect(() =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+        })
+        return () =>{
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {user, createUser, updateUserProfile}
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
--- src/Pages/Home/Auth/Register/Register.js
+++ src/Pages/Home/Auth/Register/Register.js
@@ -1,48 +1,76 @@
 import React from "react";
+import { useState } from "react";
+import { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../../Context/AuthProvider";
 
 const Register = () => {
+
+    const {createUser, updateUserProfile} = useContext(AuthContext);
+    const [accepted, setAccepted] = useState(false);
 
     const handleSubmit = (event) =>{
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
-        const photoURL = form.photoURL.value;
+        const photo = form.photoURL.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, photoURL, email, password)
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user); 
+            handleUpdateUserProfile(photo, name);
+            form.reset();
+        })
+        .catch(error => console.error(error.message))
     }
+
+    const handleUpdateUserProfile = (photo, name) =>{
+       const profile = {
+        displayName : name,
+        photoURL : photo 
+       }
+       updateUserProfile(profile)
+       .then(result => {})
+       .catch(error => console.log(error))
+    }
+
+    const handleChecked = (event) =>{
+       setAccepted(event.target.checked)
+    }
+ 
 
   return (
     <div className="mt-5">
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" name="name" placeholder="Full Name" /> 
+          <Form.Control type="text" name="name" placeholder="Full Name" required /> 
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Photo URL</Form.Label>
-          <Form.Control type="text" name="photoURL" placeholder="Enter Photo URL" /> 
+          <Form.Control type="text" name="photoURL" placeholder="Enter Photo URL" required /> 
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter email" /> 
+          <Form.Control type="email" name="email" placeholder="Enter email" required /> 
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" placeholder="Password" />
+          <Form.Control type="password" name="password" placeholder="Password" required />
         </Form.Group>
 
         <Link to='/login'><p>New To The Website ? Login Please</p></Link>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" onClick={handleChecked} label="Check me out" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" disabled={!accepted}>
           Register
         </Button>
       </Form>
