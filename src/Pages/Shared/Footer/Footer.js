d05afd48dbac2c7fc1db53b6fa8fd5ad7b5cc231
Tarikulk <ullahtarik300@gmail.com>
2022-10-27 15:02:17 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,13 +1,123 @@
 import React from 'react';
+import "./Footer.css"
+import logo from "../../../assets/logo.png"
+import { Link } from 'react-router-dom';
 
 
 const Footer = () => {
     return (
-        <div>
-          
-
-
+        <footer className="footer-section">
+        <div className="container">
+            <div className="footer-cta pt-5 pb-5">
+                <div className="row">
+                    <div className="col-xl-4 col-md-4 mb-30">
+                        <div className="single-cta">
+                            <i className="fas fa-map-marker-alt"></i>
+                            <div className="cta-text">
+                                <h4>Find us</h4>
+                                <span>1010 Avenue, sw 54321, chandigarh</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-md-4 mb-30">
+                        <div className="single-cta">
+                            <i className="fas fa-phone"></i>
+                            <div className="cta-text">
+                                <h4>Call us</h4>
+                                <span>9876543210 0</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-md-4 mb-30">
+                        <div className="single-cta">
+                            <i className="far fa-envelope-open"></i>
+                            <div className="cta-text">
+                                <h4>Mail us</h4>
+                                <span>mail@info.com</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-content pt-5 pb-5">
+                <div className="row">
+                    <div className="col-xl-4 col-lg-4 mb-50">
+                        <div className="footer-widget">
+                            <div className="footer-logo">
+                                <Link to="/"><img src={logo} className="img-fluid rounded-circle w-25 h-25" alt="logo"/></Link>
+                            </div>
+                            <div className="footer-text">
+                                <p>Take your desired course. Best wishes from Course-Hub</p>
+                            </div>
+                            <div className="footer-social-icon">
+                                <span>Follow us</span>
+                                <Link to="/"><i className="fab fa-facebook-f facebook-bg"></i></Link>
+                                <Link to="/"><i className="fab fa-twitter twitter-bg"></i></Link>
+                                <Link to="/"><i className="fab fa-google-plus-g google-bg"></i></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div className="footer-widget">
+                            <div className="footer-widget-heading">
+                                <h3>Useful Links</h3>
+                            </div>
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/">about</Link></li>
+                                <li><Link to="/">services</Link></li>
+                                <li><Link to="/">portfolio</Link></li>
+                                <li><Link to="/">Contact</Link></li>
+                                <li><Link to="/">About us</Link></li>
+                                <li><Link to="/">Our Services</Link></li>
+                                <li><Link to="/">Expert Team</Link></li>
+                                <li><Link to="/">Contact us</Link></li>
+                                <li><Link to="/">Latest News</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                        <div className="footer-widget">
+                            <div className="footer-widget-heading">
+                                <h3>Subscribe</h3>
+                            </div>
+                            <div className="footer-text mb-25">
+                                <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                            </div>
+                            <div className="subscribe-form">
+                                <form action="#">
+                                    <input type="text" placeholder="Email Address"/>
+                                    <button><i className="fab fa-telegram-plane"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div className="copyright-area">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                        <div className="copyright-text">
+                            <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
+                        </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                        <div className="footer-menu">
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/">Terms</Link></li>
+                                <li><Link to="/">Privacy</Link></li>
+                                <li><Link to="/">Policy</Link></li>
+                                <li><Link to="/">Contact</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     )
 };
 
